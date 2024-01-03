6a4d96846005e568586bc97ca3da282e144b1fbd
mehadiHasanDiner <mehadi.cosmo@gmail.com>
2024-01-03 17:25:47 +0600

    bistro boss module 75 part-2 running

---
+++ src/pages/Menu/Menu.jsx
@@ -0,0 +1,14 @@
+import { Helmet } from "react-helmet-async";
+const Menu = () => {
+  return (
+    <div>
+      <Helmet>
+        <title> Bistro Boss | Menu </title>
+      </Helmet>
+
+      <h2>Men men Menu</h2>
+    </div>
+  );
+};
+
+export default Menu;
