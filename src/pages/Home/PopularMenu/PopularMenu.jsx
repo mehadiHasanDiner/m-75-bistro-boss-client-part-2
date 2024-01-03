73edf660920a284ccf25c8527384b3fde5049d24
mehadiHasanDiner <mehadi.cosmo@gmail.com>
2024-01-03 21:08:33 +0600

    custom hook implemented, done

--- src/pages/Home/PopularMenu/PopularMenu.jsx
+++ src/pages/Home/PopularMenu/PopularMenu.jsx
@@ -1,17 +1,12 @@
-import { useEffect, useState } from "react";
 import SectionTitle from "../../../components/SectionTitle/SectionTitle";
 import MenuItem from "../../../Shared/MenuItem/MenuItem";
+import useMenu from "../../../hooks/useMenu";
 
 const PopularMenu = () => {
-  const [menu, setMenu] = useState([]);
-  useEffect(() => {
-    fetch("menu.json")
-      .then((res) => res.json())
-      .then((data) => {
-        const popularItems = data.filter((item) => item.category === "popular");
-        setMenu(popularItems);
-      });
-  }, []);
+  const [menu] = useMenu();
+
+  const popularItems = menu.filter((item) => item.category === "popular");
+
   return (
     <section className="mb-12">
       <SectionTitle
@@ -20,7 +15,7 @@ const PopularMenu = () => {
       ></SectionTitle>
 
       <div className="grid md:grid-cols-2 gap-14">
-        {menu.map((item) => (
+        {popularItems.map((item) => (
           <MenuItem key={item._id} item={item}></MenuItem>
         ))}
       </div>
--- src/pages/Menu/Menu/Menu.jsx
+++ src/pages/Menu/Menu/Menu.jsx
@@ -1,6 +1,6 @@
 import { Helmet } from "react-helmet-async";
 import Cover from "../../../Shared/Cover/Cover";
-import coverImage from "../../../src/assets/menu/banner3.jpg";
+import coverImage from "../../../assets/menu/banner3.jpg";
 
 const Menu = () => {
   return (
